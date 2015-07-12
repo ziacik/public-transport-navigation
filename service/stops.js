889651b1d8a8d3d51d052539c6cc87fd2bdfe43b
František Žiačik <ziacik@gmail.com>
2015-07-12 23:45:43 +0200

    Added line and direction info to stops.

--- service/stops.js
+++ service/stops.js
@@ -48,21 +48,33 @@ var stopsAround = function(req, res, next) {
 
 
 var getStopsAround = module.exports.getStopsAround = function(lat, lon, distance) {	
-	//TODO To include all instances of the stop?
 	var extension = 0;
 	
 	var point = new GeoPoint(lat, lon);
-	var bounds = point.boundingCoordinates((distance + extension) / 1000.0, undefined, true);
 	
-	return overpass('[out:json];node["highway"="bus_stop"]["name"](' + bounds[0]._degLat + ',' + bounds[0]._degLon + ',' + bounds[1]._degLat + ',' + bounds[1]._degLon + ');out body;')
-	.then(function(data) {		
+	return overpass('[out:json];(node(around:' + distance + ',' + lat + ',' + lon + ')["highway"="bus_stop"]["name"];rel(bn););out body;')
+	.then(function(data) {
+		//console.log(JSON.stringify(data, null, '\t'));
 		var stops = data.features.slice()
 		.filter(function(feature) {
 			return isNaN(parseInt(feature.properties.tags.name));
 		}).map(function(feature) {
 			var stopPoint = new GeoPoint(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
 			feature.distance = Math.round(1000 * point.distanceTo(stopPoint, true));
-			feature.points = [ { lat : stopPoint.latitude(), lon : stopPoint.longitude(), distance : feature.distance } ];
+			var featurePoint = { 
+				lat : stopPoint.latitude(), 
+				lon : stopPoint.longitude(), 
+				distance : feature.distance
+			};
+			
+			if (feature.properties.relations && feature.properties.relations.length) {
+				featurePoint.lines = {};
+				feature.properties.relations.forEach(function(relation) {					
+					featurePoint.lines[relation.reltags.ref] = relation.reltags.to ? relation.reltags.to : '?';
+				});
+			}
+
+			feature.points = [ featurePoint ];
 			return feature;
 		}).sort(function(feature1, feature2) {			
 			var stopName1 = feature1.properties.tags.name;
@@ -87,11 +99,10 @@ var getStopsAround = module.exports.getStopsAround = function(lat, lon, distance
 			
 			return arr;
 		},[]).sort(function(feature1, feature2) {
-			return feature1.distance - feature2.distance;
-		}).filter(function(feature) {
-			return feature.distance <= distance;
+			return feature1.distance - feature2.distance;		
 		}).map(function(feature) {
 			return {
+				//f : feature,
 				name : feature.properties.tags.name,
 				distance : feature.distance,
 				points : feature.points
